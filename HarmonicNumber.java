7b9b156be81d17957f75a3bff46bd6525c87985a
Swapnil639 <swapnilpatil631997@gmail.com>
2022-08-17 17:54:00 +0530

    Print Harmonic number

---
+++ HarmonicNumber.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        //Hn = 1+ 1/2 + 1/3 + .........+1/n
+        System.out.print("Enter n value : ");
+        Scanner sc = new Scanner(System.in);
+        double n=sc.nextInt();
+        double sum=0;
+        while(n>0){
+            sum=sum+(1 / n);
+            n--;
+
+        }
+        System.out.println(" Harmonic value is :"+sum);
+    }
+
+}
