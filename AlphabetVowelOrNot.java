7b3dbc1c231761496abbca105af3d8d71e8e3af9
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-08-17 18:07:52 +0530

    Added :Find Alphabet or vowels

---
+++ AlphabetVowelOrNot.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AlphabetVowelOrNot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String alphabet=scanner.next();
+        switch (alphabet){
+            case "a" : System.out.println("a is Vowel");break;
+            case "e" : System.out.println("e is Vowel");break;
+            case "i" : System.out.println("i is Vowel");break;
+            case "o" : System.out.println("o is Vowel");break;
+            case "u" : System.out.println("u is Vowel");break;
+            case "A" : System.out.println("A is Vowel");break;
+            case "E" : System.out.println("E is Vowel");break;
+            case "I" : System.out.println("I is vowel");break;
+            case "O" : System.out.println("O is Vowel");break;
+            case "U" : System.out.println("U is Vowel");break;
+            default: System.out.println(alphabet+ " is Consonant");break;
+    }
+}
+}
