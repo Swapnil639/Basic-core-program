d618c4b91996c156f48ab6299476eef451e6fa89
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-08-17 18:05:31 +0530

    Added :swap number

---
+++ SwapNumber.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        int a,b,c;
+        System.out.print("Enter two number :");
+        Scanner sc = new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("Before swapping :"+a+ " "+b);
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("After swapping :"+a+ " "+b);
+
+    }
+}
